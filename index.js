bf82b61d83591fc91dbc5aafc366e8a5bb18e645
Joeri Van Hoof <jvh@jvh.be>
2021-11-27 16:21:54 +0100

    Added input and output

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const github = require('@actions/github');
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const flexVmAppId = core.getInput('FLEXVM_APP_ID');
-  const flexVmPassword = core.getInput('FLEXVM_PASSWORD');
+  const flexVmAppId = core.getInput('flexvm_app_id');
+  const flexVmPassword = core.getInput('flexvm_password');
   console.log(`Hello ${flexVmAppId}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("flexvm_token", time);
-  core.setOutput("flexvm_license", time);
+  core.setOutput("flexvm_serial_number", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
